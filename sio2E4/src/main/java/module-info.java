1c136a300631ae883125abf47f1719d1fdd93581
tizba <baptou.audeon@gmail.com>
2023-10-09 08:17:03 +0200

    début page de connexion system

--- sio2E4/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.openjfx.sio2E4 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires transitive javafx.graphics;
-
-    opens org.openjfx.sio2E4 to javafx.fxml;
-    exports org.openjfx.sio2E4;
-}
